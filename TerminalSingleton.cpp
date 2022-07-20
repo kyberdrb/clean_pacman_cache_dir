1ca4a474b6f25d4b75d5601d6c70de9680b667de
kyberdrb <kyberdrb@gmail.com>
2022-07-20 16:12:14 +0200

    Singleton (design pattern) with static local variable of smart pointer type with 'std::make_unique' as friend function...
    ...in order to access the private constructor of the Singleton class
    - allocating the Singleton instance on the 'heap' is useful for Singleton instances that are larger than the available free space on the stack (which leads to 'StackOverflow' error) and need more space in operational memory (RAM)

--- TerminalSingleton.cpp
+++ TerminalSingleton.cpp
@@ -1,15 +1,19 @@
 //
-// Created by laptop on 7/16/22.
+// Created by laptop on 7/19/22.
 //
 
 #include "TerminalSingleton.h"
 
 #include <iostream>
 
-std::unique_ptr<TerminalSingleton> TerminalSingleton::theOneAndOnlyTerminalSingletonInstance;
-
 const TerminalSingleton& TerminalSingleton::get() {
-    return *(TerminalSingleton::theOneAndOnlyTerminalSingletonInstance);
+    // Making the 'std::make_unique' a friend of this class fixes the error message in clang-tidy:
+    //     "calling a private constructor of class 'TerminalSingleton'"
+    //  and its related error message from the compiler
+    //     "error: ‘constexpr TerminalSingleton::TerminalSingleton()’ is private within this context"
+    static std::unique_ptr<TerminalSingleton> theOneAndOnlyTerminalSingletonInstance = std::make_unique<TerminalSingleton>();
+
+    return *theOneAndOnlyTerminalSingletonInstance;
 }
 
 const TerminalSingleton& TerminalSingleton::printText(const std::string& text) const {
--- TerminalSingleton.h
+++ TerminalSingleton.h
@@ -1,5 +1,5 @@
 //
-// Created by laptop on 7/16/22.
+// Created by laptop on 7/19/22.
 //
 
 #pragma once
@@ -9,29 +9,26 @@
 
 class TerminalSingleton {
 public:
+    // Enables the 'std::make_unique' to access the private constructor of this class
+    //  so that the instance of this class can be created with 'std::make_unique' only from within of this class
+    //  and not from outside.
+    //  I like this version of the Singleton implementation better, because
+    //    1. sufficiency - only the 'get' function is accessible from outside - from the public static interface of the class
+    //    2. humbleness - only the class itself is needed - no derived classes i.e. no inheritance or polymorphism
+    friend std::unique_ptr<TerminalSingleton> std::make_unique<TerminalSingleton>();
     static const TerminalSingleton& get();
 
     // Return reference to the instance to enable Fluent Interface, i.e. function chaining,
     //  to print text continuously
     const TerminalSingleton& printText(const std::string& text) const;
-    void printText(const std::stringstream& textstream) const;
+    void printText(const std::stringstream& textStream) const;
 
-    // Disable copy semantics:
-    //   - disable copy constructor
     TerminalSingleton(const TerminalSingleton& otherTerminalSingleton) = delete;
-
-    //   - disable copy assignment operator
     TerminalSingleton& operator=(const TerminalSingleton& otherTerminalSingleton) = delete;
 
-    // Disable move semantics
-    //   - disable move constructor
     TerminalSingleton(TerminalSingleton&& otherTerminalSingleton) = delete;
-
-    //   - disable move assignment operator
     TerminalSingleton& operator=(TerminalSingleton&& otherTerminalSingleton) = delete;
 
 private:
-    static std::unique_ptr<TerminalSingleton> theOneAndOnlyTerminalSingletonInstance;
-
     TerminalSingleton() = default;
 };
