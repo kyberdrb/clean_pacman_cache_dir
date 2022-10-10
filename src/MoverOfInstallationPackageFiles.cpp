ad7ceaff07701ea638a38bc07fbc3915428ca65e
kyberdrb <kyberdrb@gmail.com>
2022-10-10 02:43:57 +0200

    Sanitize memory leak at creating directory for package related files for deletion in system space as a regular user...
    ...without elevated permissions as 'sudo' or 'root' user
    
    Ah, such a relief. Satisfaction.

--- src/MoverOfInstallationPackageFiles.cpp
+++ src/MoverOfInstallationPackageFiles.cpp
@@ -27,12 +27,30 @@ void MoverOfInstallationPackageFiles::moveChosenInstallationPackageFilesToSepara
 
     TerminalAndLoggerSingleton::get().printAndLog(message.str());
 
-    std::filesystem::create_directories(Paths::get().getPathToDirectoryForInstallationPackageFilesDeletionCandidatesAsText());
+    message.str("");
+    message.clear();
 
-    const auto directoryForInstallationPackageFilesForDeletion =
-            std::make_unique<AbsolutePath>(Paths::get().getPathToDirectoryForInstallationPackageFilesDeletionCandidatesAsText());
+    std::string dividingDashedLine = "---\n";
 
-    this->locallyInstalledPackages.movePackageFilesForDifferentPackageVersionsToSeparateDir(*directoryForInstallationPackageFilesForDeletion);
-    this->packageFilesRelatedToPackages.moveChosenInstallationPackageFiles(*directoryForInstallationPackageFilesForDeletion);
-    this->packageFilesRelatedToPackages.cleanUpOtherFilesInPikaurCacheDirs(*directoryForInstallationPackageFilesForDeletion);
+    // catch 'std::filesystem' exception to prevent memory leaks when the user doesn't have the priviledges to create directory
+    try {
+        std::filesystem::create_directories(Paths::get().getPathToDirectoryForInstallationPackageFilesDeletionCandidatesAsText());
+
+        const auto directoryForInstallationPackageFilesForDeletion =
+                std::make_unique<AbsolutePath>(Paths::get().getPathToDirectoryForInstallationPackageFilesDeletionCandidatesAsText());
+
+        if (std::filesystem::exists(directoryForInstallationPackageFilesForDeletion->getAbsolutePath())) {
+            this->locallyInstalledPackages.movePackageFilesForDifferentPackageVersionsToSeparateDir(*directoryForInstallationPackageFilesForDeletion);
+            this->packageFilesRelatedToPackages.moveChosenInstallationPackageFiles(*directoryForInstallationPackageFilesForDeletion);
+            this->packageFilesRelatedToPackages.cleanUpOtherFilesInPikaurCacheDirs(*directoryForInstallationPackageFilesForDeletion);
+        }
+    } catch (const std::filesystem::__cxx11::filesystem_error& exception) {
+        message
+                << exception.what() << "\n"
+                << "Error: Insufficient permissions to create directory for package related files for deletion." << "\n"
+                << "Please, run this program with elevated priviledges as 'sudo' or 'root' user." << "\n"
+                << dividingDashedLine;
+
+        TerminalAndLoggerSingleton::get().printAndLog(message.str());
+    }
 }
